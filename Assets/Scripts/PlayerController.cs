3da97e4d4fde7be259a13574a479d88fee09f6fe
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-10 04:20:13 +0300

    power up and countdown added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     public float speed = 1;
     private GameObject focalPoint;
 
+    private bool hasPowerup;
+    private float powerupStrength = 15;
+
+    public GameObject powerupIndicator;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -19,5 +24,39 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed *  forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            powerupIndicator.gameObject.SetActive(true);
+            StartCoroutine("PowerupCountdownRoutine");
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        powerupIndicator.gameObject.SetActive(false);
+        hasPowerup = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to: " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+
+
+        }
     }
 }
